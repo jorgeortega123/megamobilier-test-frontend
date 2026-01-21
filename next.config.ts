6ef189ac414d0d1718e963e8f809f7ca9b2a047e
jorgeortega123 <garridomateo74@gmail.com>
2026-01-21 00:39:00 -0600

    Exportar páginas de manera estática agregado

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
   reactStrictMode: true,
+  output: "export",
 };
 
 export default nextConfig;
